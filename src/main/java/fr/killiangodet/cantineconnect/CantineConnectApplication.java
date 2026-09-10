8c7a8709c0951ffa85f4dc4f0f7a57937ef85340
Killian Godet <godetkillian@outlook.com>
2026-09-10 18:26:51 +0200

    chore(project): initialize Spring Boot application structure
    Add initial dependencies:
    - Spring Web
    - Lombok
    - Spring Boot Docker Compose
    - SpringDoc OpenAPI

---
+++ src/main/java/fr/killiangodet/cantineconnect/CantineConnectApplication.java
@@ -0,0 +1,13 @@
+package fr.killiangodet.cantineconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CantineConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CantineConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/killiangodet/cantineconnect/CantineConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.killiangodet.cantineconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CantineConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
